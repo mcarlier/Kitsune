09906ca962e49e3034c2f887756bd3ecb2aa7089
mcarlier <maelle-carlier@hotmail.fr>
2017-05-18 10:09:47 +0200

    Gestion de la navigation data-action

--- src/app.js
+++ src/app.js
@@ -1,30 +1,34 @@
-// Here is the starting point for your application code.
+var $ = require("jquery");
+
+var status;
 
 // Small helpers you might want to keep
 import './helpers/context_menu.js';
 import './helpers/external_links.js';
 
 // All stuff below is just to show you how it works. You can delete all of it.
-import { remote } from 'electron';
-import jetpack from 'fs-jetpack';
 import { greet } from './hello_world/hello_world';
-import env from './env';
+import { remote } from 'electron';
+
 
 const app = remote.app;
-const appDir = jetpack.cwd(app.getAppPath());
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files form disk like it's node.js! Welcome to Electron world :)
-const manifest = appDir.read('package.json', 'json');
 
-const osMap = {
-  win32: 'Windows',
-  darwin: 'macOS',
-  linux: 'Linux',
-};
+//const manifest = appDir.read('package.json', 'json');
 
-document.querySelector('#greet').innerHTML = greet();
-document.querySelector('#os').innerHTML = osMap[process.platform];
-document.querySelector('#author').innerHTML = manifest.author;
-document.querySelector('#env').innerHTML = env.name;
-document.querySelector('#electron-version').innerHTML = process.versions.electron;
+
+// document.querySelector('#greet').innerHTML = greet();
+
+// $(".classRollTarget").click(function() {
+//   alert(this.getAttribute('data-user-status'));
+//   status = this.getAttribute('data-user-status');
+// });
+
+$(document).on('click', 'button', function(e) {
+  if(this.getAttribute('data-target')){
+    $(".container").load(app.getAppPath()+"/app"+this.getAttribute('data-target'));
+  }
+  if(this.getAttribute('data-user-status')){
+    status = this.getAttribute('data-user-status');
+  }
+})
